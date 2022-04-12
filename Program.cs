a1b4bafbfecd33874a5422f200cd11848b141a7a
Lussier07 <brendan.lussier07@gmail.com>
2022-04-11 23:39:03 -0500

    Add method to replace periods from a file with the word STOP, and create a new file from that replaced file.

--- Program.cs
+++ Program.cs
@@ -1,5 +1,69 @@
 ﻿using System.IO;
 
 
+string dir = @"C:\Users\brend\Documents\folda";
+string path = "";
 
+List<string> files = new List<string>();
+
+try
+{
+    Directory.SetCurrentDirectory(dir);
+    files = Directory.GetFiles(dir, "theMachineStops.txt").ToList();
+    
+    if (files.Count > 0)
+    {
+        path = files[0];
+    }
+}
+catch (IOException ex)
+{
+    Console.WriteLine($"Error: {ex.Message}");
+}
+
+
+PeriodToStop(path, dir);
+
+static void PeriodToStop(string path, string dir)
+{
+    FileInfo file = new FileInfo(path);
+
+    if (file.Exists)
+    {
+        try
+        {
+            string newTxt = "";
+            using (StreamReader reader = file.OpenText())
+            {
+                string text = reader.ReadToEnd();
+
+                string[] split = text.Split('.');
+
+                foreach (string part in split)
+                {
+                    newTxt += part + "STOP";
+                }
+            }
+
+            string telegramCopyPath = dir + @"\TelegramCopy.txt";
+
+            if (File.Exists(telegramCopyPath))
+            {
+                Console.WriteLine("File already exists");
+                return;
+            }
+
+            using (StreamWriter writer = File.CreateText(telegramCopyPath))
+            {
+                writer.Write(newTxt);
+            }
+
+            Console.WriteLine("Success");
+        }
+        catch (IOException ex)
+        {
+            Console.WriteLine($"Error: {ex.Message}");
+        }
+    }
+}
 
